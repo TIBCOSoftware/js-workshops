36794954beab9bea0871db550f8e632af164e212
anthomba-tibco <73111464+anthomba-tibco@users.noreply.github.com>
2020-10-21 15:35:54 +0530

    Update main.js

--- UrbanRoast-tutorial/modules/module1/1-1/javascripts/main.js
+++ UrbanRoast-tutorial/modules/module1/1-1/javascripts/main.js
@@ -1,13 +1,9 @@
 var locations = ["5th Street Cafe","Belmont Street Cafe","Franklin Street Cafe","Main Street Roastery and Cafe"]
 var selectedLocation = [];
 
-visualize({
-    auth: {
-        name: "jasperadmin",
-        password: "jasperadmin",
-        organization: "organization_1"
-    } 
-}, function(v) {
+initializeVisualize(initPage);
+
+function initPage(jrsConfig, v) {
     var barChart = v.adhocView({
         resource: "/public/Samples/FreshCoffee/Ad_Hoc_Views/Management/Net_Sales___Total_Cost_and_Gross_Profit",
         container: "#bar",
@@ -28,4 +24,4 @@ visualize({
         }); 
     }
     
-});
+}
